dd15e002f48baa48ae514d9fd33980a119a89814
Oriol Parra <oriol.parra@forcemanager.net>
2018-09-25 01:31:46 +0200

    feature: start project and create tabs

--- App.js
+++ App.js
@@ -1,21 +1,77 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { Ionicons } from '@expo/vector-icons';
+import { View, StatusBar } from 'react-native';
+import { Constants } from 'expo';
+import { createMaterialTopTabNavigator, createStackNavigator } from 'react-navigation';
+import reducer from './src/reducers';
+import DeckList from './src/containers/DeckList';
+import NewDeck from './src/containers/NewDeck';
+import { orange, black } from './src/utils/colors';
 
-export default class App extends React.Component {
+function UdaciStatusBar ({backgroundColor, ...props}) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
+
+const Tabs = createMaterialTopTabNavigator({
+  DeckList: {
+    screen: DeckList,
+    navigationOptions: {
+      tabBarLabel: 'Decks',
+      tabBarIcon: ({ tintColor }) => <Ionicons name='md-list' size={30} color={tintColor} />
+    },
+  },
+  NewDeck: {
+    screen: NewDeck,
+    navigationOptions: {
+      tabBarLabel: 'New Deck',
+      tabBarIcon: ({ tintColor }) => <Ionicons name='md-add' size={30} color={tintColor} />
+    },
+  },
+}, {
+  tabBarOptions: {
+    activeTintColor: orange,
+    indicatorStyle: {
+      backgroundColor: orange,
+    },
+    style: {
+      height: 56,
+      backgroundColor: black,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    },
+    animationEnabled: true,
+  }
+});
+
+const MainNavigator = createStackNavigator({
+  Home: {
+    screen: Tabs,
+    navigationOptions: {
+      header: null
+    }
+  },
+});
+
+export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Provider store={createStore(reducer)}>
+        <View style={{flex: 1}}>
+          <UdaciStatusBar backgroundColor={black} barStyle="light-content" />
+          <MainNavigator />
+        </View>
+      </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/utils/colors.js
@@ -0,0 +1,3 @@
+export const black = '#000';
+export const orange = '#ed8b00';
+export const white = '#fff';
