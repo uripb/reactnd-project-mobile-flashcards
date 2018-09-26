60c62c54f81f6ebc13c1379122c787389f7edb91
Oriol Parra <oriol.parra@forcemanager.net>
2018-09-26 02:56:58 +0200

    feature: new deck styles and components

--- src/containers/NewDeck.js
+++ src/containers/NewDeck.js
@@ -1,11 +1,55 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { orange, grey, white } from '../utils/colors';
+
+function SubmitBtn ({ onPress, disabled }) {
+  return (
+    <TouchableOpacity 
+      style={[styles.submitBtn, disabled ? styles.disabledBtn : styles.enabledBtn]}
+      onPress={onPress}
+      disabled={disabled}
+      activeOpacity={disabled ? 1 : 0.5}
+    >
+      <Text style={styles.submitBtnText}>Submit</Text>
+    </TouchableOpacity>
+  )
+};
 
 class NewDeck extends Component {
+
+  state = {
+    text: '',
+  }
+
+  submit = () => {
+    console.log(this.state.text);
+  };
+
+  onChangeText = (text) => {
+    this.setState({
+      text,
+    });
+  }
+
   render() {
+    const { text } = this.state;
+
     return (
       <View style={styles.container}>
-        <Text>New deck</Text>
+        <Text style={styles.questionLabel}>What is the title of your new deck?</Text>
+        <TextInput
+          style={styles.inputText}
+          editable={true}
+          placeholder={'Deck Title...'}
+          underlineColorAndroid={orange}
+          selectionColor={grey}
+          value={text}
+          onChangeText={this.onChangeText}
+        />
+        <SubmitBtn
+          onPress={this.submit}
+          disabled={text.length === 0}
+        />
       </View>
     );
   }
@@ -16,7 +60,45 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: 30,
+  },
+  questionLabel: {
+    fontSize: 30,
+    color: grey,
+    width: '70%',
+    textAlign: 'center',
+    marginBottom: 30,
+  },
+  inputText: {
+    width: '80%',
+    height: 40,
+    fontSize: 20,
+    paddingLeft: 10,
+    paddingBottom: 5,
+    marginBottom: 30,
+  },
+  submitBtn: {
+    backgroundColor: orange,
+    padding: 10,
+    paddingLeft: 50,
+    paddingRight: 50,
+    height: 45,
+    borderRadius: 4,
+    alignSelf: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  disabledBtn: {
+    opacity: 0.4,
+  },
+  enabledBtn: {
+    opacity: 1,
+  },
+  submitBtnText: {
+    color: white,
+    fontSize: 22,
+    textAlign: 'center',
   },
 });
 
--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,3 +1,4 @@
 export const black = '#000';
 export const orange = '#ed8b00';
 export const white = '#fff';
+export const grey = '#5f656b';
