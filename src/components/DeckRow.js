33160281fbd7b00f781d53e883ec337b96fec391
Oriol Parra <oriol.parra@forcemanager.net>
2018-09-26 18:30:59 +0200

    feature: animated deck when click

--- src/components/DeckRow.js
+++ src/components/DeckRow.js
@@ -1,19 +1,53 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
-import { grey } from '../utils/colors';
+import { StyleSheet, View, Text, TouchableOpacity, Animated } from 'react-native';
+import { grey, white, orangeLight } from '../utils/colors';
+
+const AnimatedTouchable = Animated.createAnimatedComponent(TouchableOpacity);
 
 class DeckRow extends PureComponent {
 
+  state = {
+    bgColor: new Animated.Value(0),
+  }
+
+  handleOnPress = () => {
+    const { title, onClick } = this.props;
+    const { bgColor } = this.state;
+
+    Animated.sequence([
+      Animated.timing(bgColor, {
+          duration: 200,
+          toValue: 1,
+      }),
+      Animated.timing(bgColor, {
+          duration: 200,
+          toValue: 0,
+      }),
+    ]).start(() => {
+      onClick(title);
+    });
+  }
+
   render() {
-    const { title, num, onClick } = this.props;
+    const { title, num } = this.props;
+    const { bgColor, bounceValue } = this.state;
+
+    const backgroundColor = bgColor.interpolate({
+      inputRange: [0, 1],
+      outputRange: [white, orangeLight],
+    });
+
     return (
-      <TouchableOpacity style={styles.row} onPress={() => onClick(title)}>
+      <AnimatedTouchable
+        style={[styles.row, {backgroundColor}]}
+        onPress={this.handleOnPress}
+      >
         <View>
           <Text style={styles.title}>{title}</Text>
           <Text style={styles.cards}>{num} cards</Text>
         </View>
-      </TouchableOpacity>
+      </AnimatedTouchable>
     );
   }
 }
@@ -38,7 +72,7 @@ const styles = StyleSheet.create({
     paddingLeft: 20,
     paddingRight: 20,
     width: "100%",
-    backgroundColor: "white"
+    backgroundColor: "transparent"
   },
   title: {
     fontSize: 20,
--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,5 +1,6 @@
 export const black = '#000';
 export const orange = '#ed8b00';
+export const orangeLight = '#FFA930';
 export const white = '#fff';
 export const grey = '#5f656b';
 export const red = '#f04540';
